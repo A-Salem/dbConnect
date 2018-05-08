5e73cdd5e75bfd28e1efd42ee1d13b5f2a4b93ce
Ahmed Salem <a.salem@arabhosters.com>
2018-05-08 22:03:03 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Script for MongoDB connection in NodeJS
---
+++ connect.js
@@ -0,0 +1,28 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// mongodb://[username:password@]host1[:port1]
+
+// Database Name
+const dbName = 'test';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  // db.listCollections(function(err, res){
+  //   console.log(res)
+  // })
+  // console.log(db)
+  const collection = db.collection('Scanner');
+  collection.findOne({}, function(err, res){
+    console.log(res)
+  })
+  client.close();
+});
